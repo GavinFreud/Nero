8b8e4cf33732e83b8363e30c37edff28743fa9b6
GavinFreud <ramiyer1998@gmail.com>
2017-12-25 21:52:56 -0500

    Added a bit more to voice.py, should now be able to play music (in theory, testing will commence tomorrow after I finish all of the necessary commands and helper functions)

--- voice.py
+++ voice.py
@@ -11,8 +11,34 @@ class Voice:
         self.volume = 1.0
         self.playlist = None
         self.time_elapse = None
+        self.music_server = None
 
     @commands.command(name="queue", pass_context=True, help="add link to queue")
     async def queue_insert(self, ctx, link):
+        song = link
+        self.playlist.add(song)
+        if not self.music_server:
+            self.music_server = self.get_or_take_member_channel(ctx)
+        if self.time_elapse <= 0:
+            if self.playlist.current is None:
+                pass
+            else:
+                await self.play_music(self.playlist.pop())
+
+    async def play_music(self, link):
+        trigger = self.music_server
+        if trigger is None:
+            await self.bot.say("Join a channel first fool")
+            return
+        if self.voice:
+            if self.voice.channel.id != trigger.id:
+                await self.voice.move_to(trigger)
+        else:
+            self.voice = await self.bot.join_voice_channel(trigger)
+        if self.player:
+            self.player.stop()
+        self.player = await self.voice.create_ytdl_player(link)
+        
+        
 
 
